c7ddad3e87187ef631cd4c1795950bd5507c2232
Scriptorium <137924644+ClmncePrst@users.noreply.github.com>
2023-10-05 21:38:24 +0200

    Mise à Jour du README et du CSS (V1)

--- JS/script.js
+++ JS/script.js
@@ -1,40 +1,46 @@
 // afficher une liste de livres à partir du tableau
 let bookList = [
     {
-      title: "A Game of Thrones",
-      picture: "https://placehold.co/300x400",
-      author: "George R.R. Martin",
-      description: "Lorsque Sophie, une pâtissière timide de New York, voyage à Paris pour un concours de pâtisserie, elle ne s'attend pas à rencontrer un charismatique chef français, Pierre. Leurs différences culturelles et culinaires sèment le chaos, mais est-ce que l'amour et la cuisine les uniront ?",
+      title: "Avis de décès",
+      picture: "https://m.media-amazon.com/images/I/51NdukGNqBS._SX195_.jpg",
+      author: "Zhou Haohui",
+      description: "18 avril 1984, une série de meurtres inexpliqués dans la ville de CHengdu incite la police à mettre sur pied une unité spéciale, la 4/18. Ne parvenant pas à trouver le coupable, l'unité est dissoute. Vingt-deux ans plus tard, un des policiers est toujours obsédé par cette affaire. Mais au moment où il pense enfin tenir un indice majeur, il est assassiné. L'Unité 4/18 renaît alors de ses cendres.",
       statut: "read"
     },
     {
-      title: "A Clash of Kings",
-      picture: "https://placehold.co/300x400",
-      author: "George R.R. Martin",
-      description: "Le détective amateur, Emily, reçoit une invitation énigmatique pour passer la nuit dans un manoir réputé hanté. Elle et son groupe d'amis se retrouvent pris au piège dans un labyrinthe de mystères et de phénomènes paranormaux. Emily doit résoudre l'énigme du manoir pour sauver ses amis avant qu'il ne soit trop tard.",
+      title: "L'île de Yule",
+      picture: "https://www.babelio.com/couv/CVT_Lile-de-yule_3310.jpg",
+      author: "Johanna Gustawsson",
+      description: "Le coeur battant, Emma Lindhal cogne à la porte du manoir dressé sur une petite île au large de Stockholm. L'île et son manoir ont une réputation sulfureuse depuis que, neuf ans plus tôt, une adolescente a été découverte pendue à un arbre du domaine, tuée dans des circonstances affreuses. Son assassin n'a jamais été retrouvé.",
       statut: "read"
     },
     {
-      title: "A Storm of Swords" ,
-      picture: "https://placehold.co/300x400",
-      author: "George R.R. Martin",
-      description: "Dans le monde exotique de l'orchidée noire, l'experte en botanique, Amelia, se retrouve mêlée à une série de meurtres mystérieux. Elle devra démêler les fils de cette intrigue mortelle tout en veillant à ce que l'orchidée rare et précieuse ne tombe pas entre de mauvaises mains.",
-      statut: "en cours de lecture"
+      title: "L'héritier" ,
+      picture: "https://www.babelio.com/couv/CVT_Haut-Royaume-tome-2--LHeritier_2622.jpg",
+      author: "Pierre Pevel",
+      description: "Une couronne. Une épée. Un trône. Les divins l'avaient distingué. Son sang était celui, maudit et glorieux, des plus grands rois qui règnèrent jamais. Mais plus que d'un trône, il était l'héritier d'un destin sacrifié.",
+      statut: "currently-reading"
     },
     {
-      title: "A Feast for Crows" ,
-      picture: "https://placehold.co/300x400",
-      author: "George R.R. Martin",
-      description: "Dans le monde exotique de l'orchidée noire, l'experte en botanique, Amelia, se retrouve mêlée à une série de meurtres mystérieux. Elle devra démêler les fils de cette intrigue mortelle tout en veillant à ce que l'orchidée rare et précieuse ne tombe pas entre de mauvaises mains.",
-      statut: "à lire" 
+      title: "L'Arabe du futur 6: Une jeunesse au Moyen-Orient (1994-2011)" ,
+      picture: "https://www.babelio.com/couv/CVT_LArabe-du-futur-tome-6--Une-jeunesse-au-Moyen-Or_8301.jpg",
+      author: "Riad Sattouf",
+      description: "L'Arabe du futur, une jeunesse au Moyen-Orient est une série de bande dessinée en six tomes, écrite et dessinée par Riad Sattouf. Elle raconte l'enfance et l'adolescence de l'auteur, fila aîné d'une mère française et d'un père syrien. Ce sixième tome couvre les années 1994-2011. C'est le dernier tome de la série."
     },
     {
       title: "A Dance with Dragons" ,
-      picture: "https://placehold.co/300x400",
+      picture: "https://www.babelio.com/couv/CVT_cvt_A-Dance-with-Dragons-Dreams-and-Dust_1642.jpg",
       author: "George R.R. Martin",
-      description: "Dans le monde exotique de l'orchidée noire, l'experte en botanique, Amelia, se retrouve mêlée à une série de meurtres mystérieux. Elle devra démêler les fils de cette intrigue mortelle tout en veillant à ce que l'orchidée rare et précieuse ne tombe pas entre de mauvaises mains.",
-      statut: "à lire"
+      description: "Entre Winterfell, Meereen et Port-Réal, les intrigues n'en finissent plus à Westeros. Et l'hiver approche. ",
+      statut: "toread"
     },
+    {
+    title: "1793" ,
+    picture: "https://www.babelio.com/couv/CVT_1793_1289.jpg",
+    author: "Niklas Natt och Dag",
+    description: "1793. Le vent de la Révolution française souffle sur les monarchies du nord. Un an après la mort du roi Gustav III de Suède, la tension est palpable dans tout le pays. Rumeurs de conspirations, paranoïa, le pays est en effervescence. Jean Michael Cardell va bientôt devoir affronter le mal et la corruption qui règnent à tous les échelons de la société suédoise, pour mettre à jour une sombre et terrible réalité.",
+    statut: "read"
+   },
   ]
   
  function showBooks (status) {
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Scriptorium 
-Application en Javascript d'administration d'une collection de livres
+# Scriptorium : a digital bookshelf
+
+Prototype d'application d'administration d'une collection de livres, réalisée en JavaScript.
+
+Cette application souhaite répertorier les livres lus, et pouvoir les classer en fonction de différents critères tels que leur statut de lecture: à lire, lu, ou en cours de lecture. Ce statut doit pouvoir être facilement modifié.
+
+Il doit aussi être possible de pouvoir noter un livre, de manière claire et visuelle.
+
+
+##Fonctionnalités futures:
+- Classement des livres selon leur note,
+- Classement des livres par genre, thèmes, etc.
+- Création d'étagères par auteurs, thème, éditeur, etc.
+- Répertoriage des statistiques: nombre de livres lus sur une période (mois, année), comparaison entre les périodes, proportion des genres dans le total des lectures, etc.
+
+
